40684eb197deb06422d085d000bcd5753e61673b
Sebastian Pietrzak <sebastian.pietrzak@giraffestudioapps.com>
2024-08-28 20:08:57 +0200

    fix:ollamaService

--- src/ollamaService.ts
+++ src/ollamaService.ts
@@ -1,11 +1,14 @@
 import axios from 'axios';
 
+interface IApiResponse {
+  models: string[];
+}
 
 const ollamaApiUrl = process.env.OLLAMA_API_URL ?? 'http://localhost:11434';
 
 export async function isModelAvailable(model: string): Promise<boolean> {
   try {
-    const response = await axios.get(`${ollamaApiUrl}/api/tags`);
+    const response = await axios.get<IApiResponse>(`${ollamaApiUrl}/api/tags`);
     const availableModels = response.data.models;
         if (Array.isArray(availableModels)) {
       return availableModels.includes(model);
